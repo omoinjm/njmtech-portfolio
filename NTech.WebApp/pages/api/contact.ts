3eb1e50b22941d30ee3cd7c352fe0b3511e39517
omoinjm <njmcloud@gmail.com>
2024-02-26 21:19:28 +0200

    hello

--- NTech.WebApp/pages/api/contact.ts
+++ NTech.WebApp/pages/api/contact.ts
@@ -1,11 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import nodemailer from "nodemailer";
+import fs from "fs";
+import path from "path";
 
-export default function handler(req: any, res: any) {
+export default function handler(req, res) {
   const transporter = nodemailer.createTransport({
     service: "gmail",
-    port: 465,
-    secure: true,
     auth: {
       user: process.env.NEXT_PUBLIC_EMAIL_USER,
       pass: process.env.NEXT_PUBLIC_EMAIL_APP_PASS,
@@ -15,35 +14,39 @@ export default function handler(req: any, res: any) {
   transporter.verify((error) => {
     if (error) {
       console.log(error);
+      res.status(500).json({ error: "Error connecting to email service" });
     } else {
       console.log("Ready to Send");
     }
   });
 
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
+  const { firstName, lastName, email, message, phone } = req.body;
+
+  const fullName =
+    firstName && lastName ? `${firstName} ${lastName}` : "Unknown";
+
+  // Read HTML template file
+  const templatePath = path.join(__dirname, "/public/index.html");
+  const htmlTemplate = fs.readFileSync(templatePath, "utf-8");
 
   const mailOptions = {
-    from: `NJMTech NoReply ${email}`,
+    from: `NJMTech NoReply <${process.env.NEXT_PUBLIC_EMAIL_USER}>`,
     to: process.env.NEXT_PUBLIC_EMAIL_MAIL,
     subject: "Contact form submission - Portfolio",
-    text: message + " | Sent from: " + email,
-    html: `<p><strong>Name: </strong> ${name} </p><br>
-      <p><strong>Email: </strong> ${email} </p><br>
-      <p><strong>Phone: </strong> ${phone} </p><br>
-      <p><strong>Message: </strong> ${message} </p><br>
-    `,
+    html: htmlTemplate
+      .replace("{{name}}", fullName)
+      .replace("{{email}}", email)
+      .replace("{{message}}", message)
+      .replace("{{phone}}", phone),
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
-      res.json("Error: " + JSON.stringify(error));
+      res.status(500).json({ error: "Error sending email" });
     } else {
       console.log("Email sent: " + info.response);
-      res.json({ code: 200, status: "Message Sent" });
+      res.status(200).json({ message: "Email sent successfully" });
     }
   });
 }
