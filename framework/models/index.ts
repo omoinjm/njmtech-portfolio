9534078b0468898a53537e0f70614e963969e111
omoinjm <njmcloud@gmail.com>
2024-10-18 15:40:42 +0000

    age model fixed

--- framework/models/index.ts
+++ framework/models/index.ts
@@ -1,17 +1,9 @@
+import { AgeModel } from "./age_model";
 import { ContactFormModel } from "./contact_form_model";
 import { FooterModel, LinkModel, MenuModel } from "./link_model";
 import { MessageLogModel } from "./message_log_model";
 import { TabProjectModel } from "./project_model";
 import { SkillModel } from "./skill_models";
 
-type AgeModel = {
-  age: number;
-  today: Date;
-  birthDate: Date;
-};
-
 export type { AgeModel, ContactFormModel, FooterModel, LinkModel, MenuModel, MessageLogModel, SkillModel, TabProjectModel };
-
-
-
 
